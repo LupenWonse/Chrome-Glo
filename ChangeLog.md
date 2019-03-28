6570d0cf502af89b5b43889d6426239a289c25a1
Ahmet Gencoglu <ahmetgencoglu@gmail.com>
2019-03-28 13:00:43 +0300

    Fixes #1 Move all info under Description
    No longer using comments

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+v1.1
+
+- Created cards no longer have comments. Everything is packed away nicely under Description
--- gloAPI.js
+++ gloAPI.js
@@ -1,3 +1,24 @@
+function updateCard(card) {
+    console.log("CARD");
+    console.log(card);
+    var xhr = new XMLHttpRequest();
+    
+    return new Promise (function(resolve, reject){
+        
+        xhr.withCredentials = false;
+        xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log("RESOLVE");
+            resolve(JSON.parse(xhr.responseText));
+        }
+        }); 
+        xhr.open("POST", "https://gloapi.gitkraken.com/v1/glo/boards/" + boardId + "/cards/"+card.id+"?access_token=" + accessToken);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("cache-control", "no-cache");
+        xhr.send(JSON.stringify(card)); 
+    });
+}
+
 function postCard(card) {
     console.log("CARD");
     console.log(card);
@@ -40,30 +61,31 @@ function postAttachment(cardId, blob){
 
 function addCard (click) {
     var card = {};
-    card.description = {};
+    card.description= {text: ""};
     card.position = 0;
     card.column_id = click.menuItemId;
+    card.id = '';
     
     var currentTab;
-    var createdCardId;
+    var createdCard;
     
     var promise = getCurrentTab().then(tab => {
         currentTab = tab;
         if (click.selectionText){
             if (click.selectionText.length < 90){
                 card.name = click.selectionText;
-                card.description.text = " \n Created by Chrome Glo \n" + currentTab.url;
+                
             } else {
                 
                 card.name = currentTab.title;
                 console.log('NAME : ' + card.name);
                 card.description.text = click.selectionText;
-                card.description.text += " \n Created by Chrome Glo \n" + currentTab.url;
             }
         } else {
             card.name = currentTab.title;
-            card.description.text = " \n Created by Chrome Glo \n" + currentTab.url;
         }
+        card.description.text += "\n Card Created by Chrome Glo";
+        card.description.text += '\n [Original web-page](' + currentTab.url +')\n';
     });
     
     if (click.mediaType == "image"){
@@ -80,14 +102,14 @@ function addCard (click) {
     })
     .then(function(responseData){
         console.log(responseData);
-       createdCardId = responseData.id;
+       createdCard = responseData;
     })
     .then(function(){
         return getLocalData('shouldShowCard');
     })
     .then(function(shouldShowCard){
         if (shouldShowCard){
-            chrome.tabs.create({"url": 'https://app.gitkraken.com/glo/board/' + boardId + '/card/' + createdCardId});
+            chrome.tabs.create({"url": 'https://app.gitkraken.com/glo/board/' + boardId + '/card/' + createdCard.id});
         }
     })
     .then(function(){
@@ -95,13 +117,13 @@ function addCard (click) {
     })
     .then(res => res.blob())
     .then(function(blob){
-        return postAttachment(createdCardId, blob);
+        return postAttachment(createdCard.id, blob);
     })
     .then(function(attachmentData){
-        var comment = 'Card created by Glo Chrome extension \n';
-        comment = comment + '[Original web-page](' + currentTab.url +')\n';
-        comment = comment + '![image](' + attachmentData.url + ')';
-        addComment(boardId, createdCardId, comment);
+        createdCard.description.text = '![image](' + attachmentData.url + ')\n' + createdCard.description.text;
+    })
+    .then(function(){
+        updateCard(createdCard);
     })
     .catch(function(error){
         console.error("Add Card Failed : " + error);
